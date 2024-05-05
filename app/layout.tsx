248647aead98767a39315400fe664f2944af8816
kurodenjiro <kurodenjiro1@gmail.com>
2024-05-06 00:18:25 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,29 @@
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
-import SessionProvider from "components/SessionProvider";
+import SessionProvider from "../components/SessionProvider";
+import { headers } from 'next/headers'
+import { cookieToInitialState } from 'wagmi'
+import { config } from './config'
+import { Providers } from './providers'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const initialState = cookieToInitialState(
+    config,
+    headers().get('cookie')
+  )
   return (
     <html lang="en" className={GeistSans.variable}>
-      <body><SessionProvider>{children}</SessionProvider></body>
+      <body>
+        <Providers initialState={initialState}> 
+        <SessionProvider>
+          {children}
+        </SessionProvider>
+        </Providers>
+      </body>
     </html>
   );
 }
