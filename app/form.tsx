f2622cfd3c27cfd2c042715597bde7ee0ce6e222
kurodenjiro <kurodenjiro1@gmail.com>
2024-05-05 20:18:15 +0700

    update

--- app/form.tsx
+++ app/form.tsx
@@ -300,10 +300,13 @@ export function PayoutVoteForm({ payout, viewResults }: { payout: Payout, viewRe
                 }}
             >
                 {state.showResults ? <PayoutResults payout={payout} /> : <PayoutOptions payout={payout} onChange={handleVote} />}
-                {state.showResults ? <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    type="submit"
-                >Back</button> :
+                {state.showResults ? <>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        type="submit"
+                    >Back</button>
+                </>
+                    :
                     <button
                         className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" + (selectedOption < 1 ? " cursor-not-allowed" : "")}
                         type="submit"
