b2b5c203b43a20e41cbf4f72707a295a43921d9f
kurodenjiro <kurodenjiro1@gmail.com>
2024-05-05 20:33:02 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ function Content() {
         {session ? (
           <button
             type="button"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={() => signOut()}
           >
             Sign out
