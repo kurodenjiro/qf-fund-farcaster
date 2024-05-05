993b25bc088f83307fe5390ffaac22c48f9b40ab
kurodenjiro <kurodenjiro1@gmail.com>
2024-05-05 15:50:41 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
-import SessionProvider from "@/components/SessionProvider";
+import SessionProvider from "../components/SessionProvider";
 
 export default function RootLayout({
   children,
